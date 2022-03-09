14ecd22ea32d2862325b2f1a0348e9fe48d77674
Artur <60693984+ithelparturgolata@users.noreply.github.com>
2022-03-09 16:01:42 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flaskext.mysql import MySQL #pip install flask-mysql
 import pymysql
 
 
-connection = mysql.connector.connect(host="localhost", port="3306", database="bazasm", user="root", password="Savakiran03")
+connection = mysql.connector.connect(host="localhost", port="3306",
+                                     database="bazasm", user="root", password="Savakiran03")
 cursor = connection.cursor()
 
 app = Flask(__name__)
@@ -48,7 +49,8 @@ def login():
         message = "Błędny login"
         username = request.form["username"]
         password = request.form["password"]
-        cursor.execute("SELECT * FROM uzytkownicy WHERE username = %s AND password = %s", (username, password))
+        cursor.execute("SELECT * FROM uzytkownicy WHERE "
+                       "username = %s AND password = %s", (username, password))
         record = cursor.fetchone()
         if record:
             session["loggedin"] = True
@@ -90,7 +92,8 @@ def ajaxfile():
 
             ## Total number of records with filtering
             likeString = "%" + searchValue + "%"
-            cursor.execute("SELECT count(*) as allcount from mieszkaniec WHERE indeks LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s",
+            cursor.execute("SELECT count(*) as allcount from mieszkaniec "
+                           "WHERE indeks LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s",
                 (likeString, likeString, likeString))
             rsallcount = cursor.fetchone()
             totalRecordwithFilter = rsallcount['allcount']
@@ -98,11 +101,212 @@ def ajaxfile():
 
             ## Fetch records
             if searchValue == '':
-                cursor.execute("SELECT * FROM mieszkaniec ORDER BY indeks asc limit %s, %s;", (row, rowperpage))
+                cursor.execute("SELECT * FROM mieszkaniec "
+                               "ORDER BY indeks asc limit %s, %s;", (row, rowperpage))
                 mieszkanieclist = cursor.fetchall()
             else:
                 cursor.execute(
-                    "SELECT * FROM mieszkaniec WHERE indeks LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s limit %s, %s;",
+                    "SELECT * FROM mieszkaniec WHERE indeks "
+                    "LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s limit %s, %s;",
+                    (likeString, likeString, likeString, row, rowperpage))
+                mieszkanieclist = cursor.fetchall()
+
+            data = []
+            for row in mieszkanieclist:
+                data.append({
+                    'indeks': row['indeks'],
+                    'nazwakarty': row['nazwaKarty'],
+                    'adresKarty': row['adresKarty'],
+                    'klatkaSchodowa': row['klatkaSchodowa'],
+                    'telefon': row['telefon'],
+                    'administracja': row['administracja'],
+                })
+
+            response = {
+                'draw': draw,
+                'iTotalRecords': totalRecords,
+                'iTotalDisplayRecords': totalRecordwithFilter,
+                'aaData': data,
+            }
+            return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route("/ajaxfilenw", methods=["POST", "GET"])
+def ajaxfilenw():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        if request.method == 'POST':
+            draw = request.form['draw']
+            row = int(request.form['start'])
+            rowperpage = int(request.form['length'])
+            searchValue = request.form["search[value]"]
+            print(draw)
+            print(row)
+            print(rowperpage)
+            print(searchValue)
+
+            ## Total number of records without filtering
+            cursor.execute("select count(*) as allcount from mieszkaniec")
+            rsallcount = cursor.fetchone()
+            totalRecords = rsallcount['allcount']
+            print(totalRecords)
+
+            ## Total number of records with filtering
+            likeString = "%" + searchValue + "%"
+            cursor.execute("SELECT count(*) as allcount from mieszkaniec "
+                           "WHERE indeks LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s",
+                (likeString, likeString, likeString))
+            rsallcount = cursor.fetchone()
+            totalRecordwithFilter = rsallcount['allcount']
+            print(totalRecordwithFilter)
+
+            ## Fetch records
+            if searchValue == '':
+                cursor.execute("SELECT * FROM mieszkaniec WHERE administracja='NW'"
+                               "ORDER BY indeks asc limit %s, %s;", (row, rowperpage))
+                mieszkanieclist = cursor.fetchall()
+            else:
+                cursor.execute(
+                    "SELECT * FROM mieszkaniec WHERE indeks "
+                    "LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s limit %s, %s;",
+                    (likeString, likeString, likeString, row, rowperpage))
+                mieszkanieclist = cursor.fetchall()
+
+            data = []
+            for row in mieszkanieclist:
+                data.append({
+                    'indeks': row['indeks'],
+                    'nazwakarty': row['nazwaKarty'],
+                    'adresKarty': row['adresKarty'],
+                    'klatkaSchodowa': row['klatkaSchodowa'],
+                    'telefon': row['telefon'],
+                    'administracja': row['administracja'],
+                })
+
+            response = {
+                'draw': draw,
+                'iTotalRecords': totalRecords,
+                'iTotalDisplayRecords': totalRecordwithFilter,
+                'aaData': data,
+            }
+            return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route("/ajaxfilens", methods=["POST", "GET"])
+def ajaxfilens():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        if request.method == 'POST':
+            draw = request.form['draw']
+            row = int(request.form['start'])
+            rowperpage = int(request.form['length'])
+            searchValue = request.form["search[value]"]
+            print(draw)
+            print(row)
+            print(rowperpage)
+            print(searchValue)
+
+            ## Total number of records without filtering
+            cursor.execute("select count(*) as allcount from mieszkaniec")
+            rsallcount = cursor.fetchone()
+            totalRecords = rsallcount['allcount']
+            print(totalRecords)
+
+            ## Total number of records with filtering
+            likeString = "%" + searchValue + "%"
+            cursor.execute("SELECT count(*) as allcount from mieszkaniec "
+                           "WHERE indeks LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s",
+                (likeString, likeString, likeString))
+            rsallcount = cursor.fetchone()
+            totalRecordwithFilter = rsallcount['allcount']
+            print(totalRecordwithFilter)
+
+            ## Fetch records
+            if searchValue == '':
+                cursor.execute("SELECT * FROM mieszkaniec WHERE administracja='NS'"
+                               "ORDER BY indeks asc limit %s, %s;", (row, rowperpage))
+                mieszkanieclist = cursor.fetchall()
+            else:
+                cursor.execute(
+                    "SELECT * FROM mieszkaniec WHERE indeks "
+                    "LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s limit %s, %s;",
+                    (likeString, likeString, likeString, row, rowperpage))
+                mieszkanieclist = cursor.fetchall()
+
+            data = []
+            for row in mieszkanieclist:
+                data.append({
+                    'indeks': row['indeks'],
+                    'nazwakarty': row['nazwaKarty'],
+                    'adresKarty': row['adresKarty'],
+                    'klatkaSchodowa': row['klatkaSchodowa'],
+                    'telefon': row['telefon'],
+                    'administracja': row['administracja'],
+                })
+
+            response = {
+                'draw': draw,
+                'iTotalRecords': totalRecords,
+                'iTotalDisplayRecords': totalRecordwithFilter,
+                'aaData': data,
+            }
+            return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route("/ajaxfilece", methods=["POST", "GET"])
+def ajaxfilece():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        if request.method == 'POST':
+            draw = request.form['draw']
+            row = int(request.form['start'])
+            rowperpage = int(request.form['length'])
+            searchValue = request.form["search[value]"]
+            print(draw)
+            print(row)
+            print(rowperpage)
+            print(searchValue)
+
+            ## Total number of records without filtering
+            cursor.execute("select count(*) as allcount from mieszkaniec")
+            rsallcount = cursor.fetchone()
+            totalRecords = rsallcount['allcount']
+            print(totalRecords)
+
+            ## Total number of records with filtering
+            likeString = "%" + searchValue + "%"
+            cursor.execute("SELECT count(*) as allcount from mieszkaniec "
+                           "WHERE indeks LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s",
+                (likeString, likeString, likeString))
+            rsallcount = cursor.fetchone()
+            totalRecordwithFilter = rsallcount['allcount']
+            print(totalRecordwithFilter)
+
+            ## Fetch records
+            if searchValue == '':
+                cursor.execute("SELECT * FROM mieszkaniec WHERE administracja='CE'"
+                               "ORDER BY indeks asc limit %s, %s;", (row, rowperpage))
+                mieszkanieclist = cursor.fetchall()
+            else:
+                cursor.execute(
+                    "SELECT * FROM mieszkaniec WHERE indeks "
+                    "LIKE %s OR nazwaKarty LIKE %s OR adresKarty LIKE %s limit %s, %s;",
                     (likeString, likeString, likeString, row, rowperpage))
                 mieszkanieclist = cursor.fetchall()
 
